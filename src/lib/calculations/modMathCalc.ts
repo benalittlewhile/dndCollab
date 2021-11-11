d77fa56874e7d5f21a85a30c404105d3815c86c7
Dewrong <ysidda33@gmail.com>
2021-11-10 20:55:55 -0600

    modifier calculator for Ability Scores

---
+++ src/lib/calculations/modMathCalc.ts
@@ -0,0 +1,29 @@
+export class Modifiers {
+  strMod: number;
+  dexMod: number;
+  conMod: number;
+  wisMod: number;
+  intMod: number;
+  chaMod: number;
+
+  constructor() {
+    this.strMod = 0;
+    this.dexMod = 0;
+    this.conMod = 0;
+    this.wisMod = 0;
+    this.intMod = 0;
+    this.chaMod = 0;
+  }
+}
+//requires the ability score to calculate the modifier
+export function asModifier(abilityScore: number) {
+  var futureMod = -5;
+  if (abilityScore < 1 || abilityScore > 30) {
+    console.log("That's not a legal ability score in 5e.");
+  } else {
+    //modifier incriments by 2 from 1 to 20 (30 for gods)
+    for (let i = 1; i < abilityScore; i = i + 2) {
+      futureMod += 1;
+    }
+  }
+}
