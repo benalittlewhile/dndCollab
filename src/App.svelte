06a44774930fc1d07aee33606dfa62b16e71ccb5
ben <isieldor@gmail.com>
2022-06-02 20:09:48 -0500

    created initial svelte app. Next step: dynamic field types

---
+++ src/App.svelte
@@ -0,0 +1,172 @@
+<script lang="ts">
+  import plus from "./assets/plus.svg";
+  import { crossfade, fly, scale, fade, slide } from "svelte/transition";
+  import type {
+    Character,
+    NumField,
+    StringField,
+    StringFieldOptional,
+    Field
+  } from "./types/character";
+
+  let addModalOpen = false;
+
+  let nameField: StringField = {
+    id: 0,
+    label: "name",
+    required: false,
+    type: "string",
+    value: "newbert",
+  };
+
+  let ageField: NumField = {
+    id: 1,
+    label: "age",
+    required: false,
+    type: "num",
+    value: 42,
+  };
+
+  let nextId = 3;
+
+  let newbert: Character = {
+    name: "newbert",
+    fields: [nameField, ageField],
+  };
+
+  const handleClick = () => {
+    const newField = {
+      id: nextId,
+      label: "height (cm)",
+      required: true,
+      type: "num",
+      value: 168,
+    } as NumField;
+  };
+  
+  const addFieldAndCloseModal = (newField: Field) => {
+    const idFixedField = {...newField, id: nextId};
+    nextId = nextId + 1;
+    newbert.fields = [...newbert.fields, idFixedField];
+    addModalOpen = false;
+  }
+</script>
+
+<main>
+  <h1>Hello Wrld</h1>
+  <h2 on:click={() => {addModalOpen = !addModalOpen}}>{newbert.name}</h2>
+  <div class="grid grid-cols-2">
+    {#each newbert.fields as field (field.id)}
+      <div
+        class="grid grid-cols-2 mx-4 my-2 p-3 bg-slate-500 rounded-md"
+        transition:fly={{ y: 200, duration: 350 }}
+      >
+        <h3 class="text-left">{field.label}</h3>
+        <!-- refactor this to a general field component -->
+        {#if field.type == "string"}
+          <input
+            type="text"
+            class="bg-slate-600"
+            required={field.required}
+            bind:value={field.value}
+          />
+        {/if}
+        {#if field.type == "num"}
+          <input
+            type="number"
+            required={field.required}
+            class="bg-slate-600"
+            bind:value={field.value}
+          />
+        {/if}
+      </div>
+    {/each}
+  </div>
+
+  <!-- <div class="flex items-center justify-center mt-6"> -->
+  <div class="context-grid">
+    {#if !addModalOpen}
+      <div class="bg-slate-600 rounded-full w-fit m-auto "
+        out:slide
+        in:fly={{y:100, delay:500}}
+      >
+        <img
+          src={plus}
+          alt="Plus icon"
+          on:click={() => {
+            addModalOpen = !addModalOpen;
+          }}
+          class="plus-button w-[30px] m-auto"
+        />
+      </div>
+      <!-- on:click={handleClick} -->
+    {:else}
+      <div
+      class="dropdown bg-slate-500 w-1/4 min-h-[30px] m-auto rounded-md py-1
+      z-20"
+        in:scale={{duration: 500}}
+        out:scale={{duration:500}}
+      >
+        {#each newbert.fields as field}
+          <h3
+          class="rounded-md bg-slate-600 m-2 p-1"
+          on:click={() => {addFieldAndCloseModal(field)}}
+          >{field.label}</h3>
+        {/each}
+      </div>
+    {/if}
+  </div>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+    width: 100%;
+    height: 100%;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 2rem auto;
+    max-width: 14rem;
+  }
+
+  input {
+    padding-left: 0.5em;
+    padding-right: 0.5em;
+    text-align: center;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+  }
+
+  .plus-button {
+    filter: invert(100%) sepia(0%) saturate(7500%) hue-rotate(154deg)
+      brightness(120%) contrast(115%);
+  }
+
+  .context-grid {
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-template-columns: 1fr;
+  }
+
+  .context-grid > * {
+    grid-row: 1;
+    grid-column: 1;
+  }
+</style>
---
+++ src/types/character.ts
@@ -0,0 +1,39 @@
+export type Character = {
+  name: string,
+  fields: Field[];
+
+}
+
+interface IField {
+  id: string|number,
+  value,
+  label: string,
+}
+export type Field = IField & (StringField | NumField)
+
+export type stringFieldRequired = IField & {
+  required: true,
+  value: string
+}
+
+export type StringFieldOptional = IField & {
+  required: false,
+  value: string|undefined;
+}
+
+export type StringField = {type: "string"} & (stringFieldRequired | StringFieldOptional);
+
+export type NumFieldRequired = IField & {
+  required: true,
+  value: number
+}
+
+export type NumFieldOptional = IField & {
+  required: false,
+  value: number|undefined;
+}
+
+export type NumField = {type: "num"} & (NumFieldOptional | NumFieldRequired);
+
+// regenerate enums at runtime for "pick one of x" fields?
+// seems like more work than just having an options array
